8283e1b4fcce21fb5e14c0e2ab3acd1c543bd551
Joana Maia <joana.maia@talkdesk.com>
2022-02-14 16:53:38 +0000

    chore: Setup reports

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,17 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+// Import commands.js using ES2015 syntax:
+import "./commands";
+import addContext from "mochawesome/addContext";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on("test:after:run", (test, runnable) => {
+  let videoName = Cypress.spec.name;
+  videoName = videoName.replace("/.js.*", ".js");
+  const videoUrl = "videos/" + videoName + ".mp4";
+
+  addContext({ test }, videoUrl);
+});
